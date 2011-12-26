3f6d120f5716c9f634bfa86e93155541f2434880
drusifer@gmail.com <drusifer@gmail.com@98586bf8-dbd0-e64e-851c-8e0e287a68f5>
2011-12-26 20:02:05 +0000

    airport edits

--- NannyMeter/src/com/dru/nm/NannyMeterActivity.java
+++ NannyMeter/src/com/dru/nm/NannyMeterActivity.java
@@ -3,6 +3,7 @@ package com.dru.nm;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.SystemClock;
@@ -22,6 +23,12 @@ public class NannyMeterActivity extends Activity {
 	private static final String STARTTIME_KEY = "STARTTIME";
 	
 	private static final String ISRUNNING_KEY = "ISRUNNING";
+
+	private static final String RATE_KEY = null;
+
+	private static final String TIP_KEY = null;
+
+	public static final int ERROR_COLOR = 0xffaaaa;
 	
 	private boolean mIsRunning = false;
 
@@ -37,10 +44,17 @@ public class NannyMeterActivity extends Activity {
 
 	private EditText mRateEditText;
 
-	private double mRate = 0.0;
-	private double mTip = 0.0;
+	private float mRate = 0.0f;
+	private int mTip = 0;
 
 	private ToggleButton mStartStop;
+
+	private TextView mRateLabel;
+
+	private TextView mTipLabel;
+    
+	public boolean mRateValid = false;
+    public boolean mTipValid = false;
 
 
 	/** Called when the activity is first created. */
@@ -55,7 +69,8 @@ public class NannyMeterActivity extends Activity {
 		mYouOweLabel = (TextView) findViewById(R.id.youowe);
 		mRateEditText = (EditText) findViewById(R.id.rate);
 		mTipEditText = (EditText) findViewById(R.id.tip);
-
+        mRateLabel = (TextView) findViewById(R.id.rateLabel);
+        mTipLabel = (TextView) findViewById(R.id.tipLabel);
 
 		mRateEditText.setOnEditorActionListener(new OnEditorActionListener() {
 			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
@@ -85,28 +100,28 @@ public class NannyMeterActivity extends Activity {
 	}
 
 	private void parseAndValidateFields() {
-		boolean rateValid = false, tipValid = false;
+		mRateValid = false; 
+		mTipValid = false;
 
 		final String rateString = mRateEditText.getText().toString();
 		try {
-			mRate = Double.parseDouble(rateString);
-			mRateEditText.setBackgroundColor(0x000000);
-			rateValid = true;
+			mRate = Float.parseFloat(rateString);
+			mRateLabel.setBackgroundColor(Color.BLACK);
+			mRateValid = true;
 		} catch (NumberFormatException e) {
-			mRateEditText.setBackgroundColor(0xffaaaa);
-
+			mRateLabel.setBackgroundColor(ERROR_COLOR);
 		}
 
 		final String tipString = mTipEditText.getText().toString();
 		try {
 			mTip = Integer.parseInt(tipString);
-			mTipEditText.setBackgroundColor(0x000000);
-			tipValid = true;
+			mTipLabel.setBackgroundColor(Color.BLACK);
+			mTipValid = true;
 		} catch (NumberFormatException e) {
-			mTipEditText.setBackgroundColor(0xffaaaa);
+			mTipLabel.setBackgroundColor(ERROR_COLOR);
 		}
 
-		mStartStop.setEnabled( rateValid && tipValid);
+		mStartStop.setEnabled(mRateValid && mTipValid);
 	}
 
 	private Handler mHandler = new Handler();
@@ -124,9 +139,9 @@ public class NannyMeterActivity extends Activity {
 
 			final double owed = (mRate/(60*60)) * (elapsed/1000);
 			final double tipCalc = owed * (mTip/100.0);
-			mYouOweLabel.setText(String.format("%0.2f", owed + tipCalc ));
+			mYouOweLabel.setText(String.format("%05.2f", owed + tipCalc ));
 
-			mTimeLabel.setText(String.format("%d:%02d:02d", hours, minutes, seconds));
+			mTimeLabel.setText(String.format("%d:%02d:%02d", hours, minutes, seconds));
 
 			mHandler.postAtTime(this,
 					start + (((minutes * 60) + seconds + 1) * 1000));
@@ -142,15 +157,20 @@ public class NannyMeterActivity extends Activity {
 		mStartTime = System.currentTimeMillis();	
 		mHandler.removeCallbacks(mUpdateTimeTask);
 		mHandler.postDelayed(mUpdateTimeTask, 100);
+		mStartTimeLabel.setText(getTimeString( mStartTime ));
+		mIsRunning = true;
+	}
+	
+	private String getTimeString( long timems ) {
 		Time t = new Time();
 		t.set(mStartTime);
-		mStartTimeLabel.setText(t.format("%I:%M:%S %p"));
-		mIsRunning = true;
+		return(t.format("%I:%M:%S %p"));
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
+        
 		if (!readInstanceState(this)) setInitialState();
 		
 		if (mIsRunning ) {
@@ -164,34 +184,59 @@ public class NannyMeterActivity extends Activity {
 
 		if (!writeInstanceState(this)) {
 			Toast.makeText(this,
-					"Failed to write state!", Toast.LENGTH_LONG).show();
+					"NannyMeter Failed to write state!", Toast.LENGTH_LONG).show();
 		}
 	}
 
-	/**
-	 * Sets the initial state of the spinner when the application is first run.
-	 */
 	public void setInitialState() {
-
+		mStartTimeLabel.setText("");
+		mRateEditText.setText("15.00");
+		mTipEditText.setText("10");
+		mStartStop.setEnabled(true);
+		mStartStop.setChecked(false);
+		mTimeLabel.setText("");
+		mYouOweLabel.setText("");
+	}
+	
+	public void setWidgetState() {
+		mStartTimeLabel.setText(getTimeString(mStartTime));
+		mRateEditText.setText(String.format("%0.2f", mRate));
+		mTipEditText.setText(String.format("%d", mTip));
+		mStartStop.setEnabled(true);
+		mStartStop.setChecked(mIsRunning);
+		parseAndValidateFields();
 	}
 
+    public void resetSavedPreferences()
+    {
+        setInitialState();
+        writeInstanceState(this);
+    }
+    
 	public boolean readInstanceState(Context c) {
-
-
-		SharedPreferences p = c.getSharedPreferences(PREFERENCES_FILE, MODE_WORLD_READABLE);
-		mStartTime = p.getLong(STARTTIME_KEY, 0);
-		mIsRunning = p.getBoolean(ISRUNNING_KEY, false);
-		return (p.contains(STARTTIME_KEY));
-
+		try {
+			SharedPreferences p = c.getSharedPreferences(PREFERENCES_FILE, MODE_WORLD_READABLE);
+			mStartTime = p.getLong(STARTTIME_KEY, 0);
+			mIsRunning = p.getBoolean(ISRUNNING_KEY, false);
+			mRate = p.getFloat(RATE_KEY, 15.00f);
+			mTip = p.getInt(TIP_KEY, 10);
+			return (p.contains(STARTTIME_KEY));
+		} catch (Exception e) {
+			e.printStackTrace();
+			Toast.makeText(this, "NannyMeter Failed to read state! " + e.getMessage(),
+					Toast.LENGTH_LONG).show();
+			return(false);
+		}
 	}
 
 	public boolean writeInstanceState(Context c) {
-
 		SharedPreferences p =
 				c.getSharedPreferences(NannyMeterActivity.PREFERENCES_FILE, MODE_WORLD_READABLE);
 		SharedPreferences.Editor e = p.edit();
 		e.putLong(STARTTIME_KEY, mStartTime);
 		e.putBoolean(ISRUNNING_KEY, mIsRunning);
+		e.putFloat(RATE_KEY, mRate);
+		e.putInt(TIP_KEY, mTip);
 		return (e.commit());
 
 	}
