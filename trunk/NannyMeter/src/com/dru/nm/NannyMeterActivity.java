7ea209ff84455abd756d1efce192f58e9929f7a0
drusifer <drusifer@98586bf8-dbd0-e64e-851c-8e0e287a68f5>
2011-12-31 02:19:08 +0000

    Got tests working

--- trunk/NannyMeter/src/com/dru/nm/NannyMeterActivity.java
+++ trunk/NannyMeter/src/com/dru/nm/NannyMeterActivity.java
@@ -3,12 +3,12 @@ package com.dru.nm;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.SystemClock;
 import android.text.format.Time;
 import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
 import android.widget.CompoundButton;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.EditText;
@@ -18,227 +18,248 @@ import android.widget.Toast;
 import android.widget.ToggleButton;
 
 public class NannyMeterActivity extends Activity {
-	private static final String PREFERENCES_FILE = "NannyMeterActivity";
+  private static final String PREFERENCES_FILE = "NannyMeterActivity";
 
-	private static final String STARTTIME_KEY = "STARTTIME";
-	
-	private static final String ISRUNNING_KEY = "ISRUNNING";
+  private static final String STARTTIME_KEY = "STARTTIME";
 
-	private static final String RATE_KEY = null;
+  private static final String ISRUNNING_KEY = "ISRUNNING";
 
-	private static final String TIP_KEY = null;
+  private static final String RATE_KEY = "RATE";
 
-	public static final int ERROR_COLOR = 0xffaaaa;
-	
-	private boolean mIsRunning = false;
+  private static final String TIP_KEY = "TIP";
 
-	private long mStartTime;
+  public static final int ERROR_COLOR = 0xffaaaa;
 
-	private TextView mTimeLabel;
+  private boolean mIsRunning = false;
 
-	private TextView mStartTimeLabel;
+  private long mStartTime;
 
-	private TextView mYouOweLabel;
+  private TextView mTimeLabel;
 
-	private EditText mTipEditText;
+  private TextView mStartTimeLabel;
 
-	private EditText mRateEditText;
+  private TextView mYouOweLabel;
 
-	private float mRate = 0.0f;
-	private int mTip = 0;
+  private EditText mTipEditText;
 
-	private ToggleButton mStartStop;
+  private EditText mRateEditText;
 
-	private TextView mRateLabel;
+  private float mRate = 0.0f;
+  private int mTip = 0;
 
-	private TextView mTipLabel;
-    
-	public boolean mRateValid = false;
-    public boolean mTipValid = false;
+  private ToggleButton mStartStop;
+
+  public boolean mRateValid = false;
+  public boolean mTipValid = false;
 
 
-	/** Called when the activity is first created. */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
 
-		mStartStop = (ToggleButton) findViewById(R.id.startstop);
-		mTimeLabel = (TextView) findViewById(R.id.time);
-		mStartTimeLabel = (TextView) findViewById(R.id.starttime);
-		mYouOweLabel = (TextView) findViewById(R.id.youowe);
-		mRateEditText = (EditText) findViewById(R.id.rate);
-		mTipEditText = (EditText) findViewById(R.id.tip);
-        mRateLabel = (TextView) findViewById(R.id.rateLabel);
-        mTipLabel = (TextView) findViewById(R.id.tipLabel);
+    mStartStop = (ToggleButton) findViewById(R.id.startstop);
+    mTimeLabel = (TextView) findViewById(R.id.time);
+    mStartTimeLabel = (TextView) findViewById(R.id.starttime);
+    mYouOweLabel = (TextView) findViewById(R.id.youowe);
+    mRateEditText = (EditText) findViewById(R.id.rate);
+    mTipEditText = (EditText) findViewById(R.id.tip);
 
-		mRateEditText.setOnEditorActionListener(new OnEditorActionListener() {
-			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
-				parseAndValidateFields();
-				return false;
-			}
-		});
+    mRateEditText.setOnEditorActionListener(new OnEditorActionListener() {
+      public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
+        parseAndValidateFields();
+        return false;
+      }
+    });
 
-		mTipEditText.setOnEditorActionListener(new OnEditorActionListener() {
-			public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
-				parseAndValidateFields();
-				return false;
-			}
-		});
+    mRateEditText.setOnFocusChangeListener(new OnFocusChangeListener() {
+      public void onFocusChange(View v, boolean hasFocus) {
+        parseAndValidateFields();
+      }
+    });
+
+    mTipEditText.setOnEditorActionListener(new OnEditorActionListener() {
+      public boolean onEditorAction(TextView arg0, int arg1, KeyEvent arg2) {
+        parseAndValidateFields();
+        return false;
+      }
+    });
+
+    mTipEditText.setOnFocusChangeListener(new OnFocusChangeListener() {
+      public void onFocusChange(View v, boolean hasFocus) {
+        parseAndValidateFields();
+      }
+    });
 
 
-		mStartStop.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+    mStartStop.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-			public void onCheckedChanged(CompoundButton buttonView,
-					boolean isChecked) {
-				if (isChecked) {
-					startTimer();
-				} else {
-					stopTimer();
-				}
-			} });
-	}
+      public void onCheckedChanged(CompoundButton buttonView,
+          boolean isChecked) {
+        if (isChecked) {
+          startTimer();
+        } else {
+          stopTimer();
+        }
+      } });
+  }
 
-	private void parseAndValidateFields() {
-		mRateValid = false; 
-		mTipValid = false;
+  private void parseAndValidateFields() {
+    mRateValid = false; 
+    mTipValid = false;
 
-		final String rateString = mRateEditText.getText().toString();
-		try {
-			mRate = Float.parseFloat(rateString);
-			mRateLabel.setBackgroundColor(Color.BLACK);
-			mRateValid = true;
-		} catch (NumberFormatException e) {
-			mRateLabel.setBackgroundColor(ERROR_COLOR);
-		}
-
-		final String tipString = mTipEditText.getText().toString();
-		try {
-			mTip = Integer.parseInt(tipString);
-			mTipLabel.setBackgroundColor(Color.BLACK);
-			mTipValid = true;
-		} catch (NumberFormatException e) {
-			mTipLabel.setBackgroundColor(ERROR_COLOR);
-		}
-
-		mStartStop.setEnabled(mRateValid && mTipValid);
-	}
-
-	private Handler mHandler = new Handler();
-
-	private Runnable mUpdateTimeTask = new Runnable() {
-
-		public void run() {
-			final long start = mStartTime;
-			final long elapsed = SystemClock.uptimeMillis() - start;
-			int seconds = (int) (elapsed / 1000);
-			int minutes = seconds / 60;
-			int hours = minutes / 60;
-			minutes = minutes % 60;
-			seconds  = seconds % 60;
-
-			final double owed = (mRate/(60*60)) * (elapsed/1000);
-			final double tipCalc = owed * (mTip/100.0);
-			mYouOweLabel.setText(String.format("%05.2f", owed + tipCalc ));
-
-			mTimeLabel.setText(String.format("%d:%02d:%02d", hours, minutes, seconds));
-
-			mHandler.postAtTime(this,
-					start + (((minutes * 60) + seconds + 1) * 1000));
-		}
-	};
-
-	protected void stopTimer() {
-		mHandler.removeCallbacks(mUpdateTimeTask);
-		mIsRunning = false;
-	}
-
-	protected void startTimer() {
-		mStartTime = System.currentTimeMillis();	
-		mHandler.removeCallbacks(mUpdateTimeTask);
-		mHandler.postDelayed(mUpdateTimeTask, 100);
-		mStartTimeLabel.setText(getTimeString( mStartTime ));
-		mIsRunning = true;
-	}
-	
-	private String getTimeString( long timems ) {
-		Time t = new Time();
-		t.set(mStartTime);
-		return(t.format("%I:%M:%S %p"));
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-        
-		if (!readInstanceState(this)) setInitialState();
-		
-		if (mIsRunning ) {
-			
-		}
-	}
-
-	@Override
-	public void onPause() {
-		super.onPause();
-
-		if (!writeInstanceState(this)) {
-			Toast.makeText(this,
-					"NannyMeter Failed to write state!", Toast.LENGTH_LONG).show();
-		}
-	}
-
-	public void setInitialState() {
-		mStartTimeLabel.setText("");
-		mRateEditText.setText("15.00");
-		mTipEditText.setText("10");
-		mStartStop.setEnabled(true);
-		mStartStop.setChecked(false);
-		mTimeLabel.setText("");
-		mYouOweLabel.setText("");
-	}
-	
-	public void setWidgetState() {
-		mStartTimeLabel.setText(getTimeString(mStartTime));
-		mRateEditText.setText(String.format("%0.2f", mRate));
-		mTipEditText.setText(String.format("%d", mTip));
-		mStartStop.setEnabled(true);
-		mStartStop.setChecked(mIsRunning);
-		parseAndValidateFields();
-	}
-
-    public void resetSavedPreferences()
-    {
-        setInitialState();
-        writeInstanceState(this);
+    final String rateString = mRateEditText.getText().toString();
+    try {
+      mRate = Float.parseFloat(rateString);
+      mRateValid = true;
+      mRateEditText.setError(null);
+    } catch (NumberFormatException e) {
+      mRateEditText.setError("You must specify a rate");
     }
-    
-	public boolean readInstanceState(Context c) {
-		try {
-			SharedPreferences p = c.getSharedPreferences(PREFERENCES_FILE, MODE_WORLD_READABLE);
-			mStartTime = p.getLong(STARTTIME_KEY, 0);
-			mIsRunning = p.getBoolean(ISRUNNING_KEY, false);
-			mRate = p.getFloat(RATE_KEY, 15.00f);
-			mTip = p.getInt(TIP_KEY, 10);
-			return (p.contains(STARTTIME_KEY));
-		} catch (Exception e) {
-			e.printStackTrace();
-			Toast.makeText(this, "NannyMeter Failed to read state! " + e.getMessage(),
-					Toast.LENGTH_LONG).show();
-			return(false);
-		}
-	}
 
-	public boolean writeInstanceState(Context c) {
-		SharedPreferences p =
-				c.getSharedPreferences(NannyMeterActivity.PREFERENCES_FILE, MODE_WORLD_READABLE);
-		SharedPreferences.Editor e = p.edit();
-		e.putLong(STARTTIME_KEY, mStartTime);
-		e.putBoolean(ISRUNNING_KEY, mIsRunning);
-		e.putFloat(RATE_KEY, mRate);
-		e.putInt(TIP_KEY, mTip);
-		return (e.commit());
+    final String tipString = mTipEditText.getText().toString();
+    try {
+      mTip = Integer.parseInt(tipString);
+      mTipValid = true;
+      mTipEditText.setError(null);
+    } catch (NumberFormatException e) {
+      mTipEditText.setError("You must specify a tip (or 0 to exclude a tip)");
+    }
 
-	}
+    mStartStop.setEnabled(mRateValid && mTipValid);
+  }
+
+  private Handler mHandler = new Handler();
+
+  public String getElapsedTimeString(long start, long end) {
+    final long elapsed = end - start;
+    int seconds = (int) (elapsed / 1000);
+    int minutes = seconds / 60;
+    int hours = minutes / 60;
+    minutes = minutes % 60;
+    seconds  = seconds % 60;
+    return String.format("%d:%02d:%02d", hours, minutes, seconds);
+  }
+
+  public String getOwedString(long start, long end) {
+    final long elapsed = end - start;
+    final double owed = (mRate/(60*60)) * (elapsed/1000);
+    final double tipCalc = owed * (mTip/100.0);
+    return String.format("%5.2f", owed + tipCalc );
+
+  }
+
+  private Runnable mUpdateTimeTask = new Runnable() {
+
+    public void run() {
+      final long start = mStartTime;
+      final long end = System.currentTimeMillis();
+
+      mYouOweLabel.setText(getOwedString(start, end));
+      mTimeLabel.setText(getElapsedTimeString(start,end));
+      mHandler.postDelayed(this, 200);
+    }
+  };
+
+  private boolean inOnResume = false;
+
+  protected void stopTimer() {
+    mHandler.removeCallbacks(mUpdateTimeTask);
+    mIsRunning = false;
+  }
+
+  protected void startTimer() {
+    if(!inOnResume) {
+      mStartTime = System.currentTimeMillis();	
+      mHandler.removeCallbacks(mUpdateTimeTask);
+      mHandler.postDelayed(mUpdateTimeTask, 100);
+      mStartTimeLabel.setText(getTimeString( mStartTime ));
+      mIsRunning = true;
+    }
+  }
+
+  private String getTimeString( long timems ) {
+    Time t = new Time();
+    t.set(mStartTime);
+    return(t.format("%I:%M:%S %p"));
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+
+    inOnResume = true;
+    if (!readInstanceState(this)) setInitialState();
+    setWidgetState();
+    if (mIsRunning ) {
+        mHandler.removeCallbacks(mUpdateTimeTask);
+        mHandler.post(mUpdateTimeTask);
+    }
+    inOnResume = false;
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+
+    if (!writeInstanceState(this)) {
+      Toast.makeText(this,
+          "NannyMeter Failed to write state!", Toast.LENGTH_LONG).show();
+    }
+  }
+
+  public void setInitialState() {
+    mStartTimeLabel.setText("");
+    mRateEditText.setText("15.00");
+    mTipEditText.setText("10");
+    mStartStop.setEnabled(true);
+    mStartStop.setChecked(false);
+    mTimeLabel.setText("");
+    mYouOweLabel.setText("");
+  }
+
+  public void setWidgetState() {
+    mStartTimeLabel.setText(getTimeString(mStartTime));
+    mRateEditText.setText(String.format("%5.2f", mRate));
+    mTipEditText.setText(String.format("%d", mTip));
+    mStartStop.setEnabled(true);
+    mStartStop.setChecked(mIsRunning);
+    parseAndValidateFields();
+  }
+
+  public void resetSavedPreferences()
+  {
+    setInitialState();
+    writeInstanceState(this);
+  }
+
+  public boolean readInstanceState(Context c) {
+    try {
+      SharedPreferences p = c.getSharedPreferences(PREFERENCES_FILE, MODE_WORLD_READABLE);
+      mStartTime = p.getLong(STARTTIME_KEY, 0);
+      mIsRunning = p.getBoolean(ISRUNNING_KEY, false);
+      mRate = p.getFloat(RATE_KEY, 15.00f);
+      mTip = p.getInt(TIP_KEY, 10);
+      return (p.contains(STARTTIME_KEY));
+    } catch (Exception e) {
+      e.printStackTrace();
+      Toast.makeText(this, "NannyMeter Failed to read state! " + e.getMessage(),
+          Toast.LENGTH_LONG).show();
+      return(false);
+    }
+  }
+
+  public boolean writeInstanceState(Context c) {
+    SharedPreferences p =
+      c.getSharedPreferences(NannyMeterActivity.PREFERENCES_FILE, MODE_WORLD_READABLE);
+    SharedPreferences.Editor e = p.edit();
+    e.putLong(STARTTIME_KEY, mStartTime);
+    e.putBoolean(ISRUNNING_KEY, mIsRunning);
+    e.putFloat(RATE_KEY, mRate);
+    e.putInt(TIP_KEY, mTip);
+    return (e.commit());
+
+  }
 
 }
--- trunk/NannyMeterTest/src/com/dru/nm/test/NannyMeterTest.java
+++ trunk/NannyMeterTest/src/com/dru/nm/test/NannyMeterTest.java
@@ -1,9 +1,12 @@
 package com.dru.nm.test;
 
+import java.util.concurrent.CyclicBarrier;
+
 import android.test.ActivityInstrumentationTestCase2;
 import android.view.KeyEvent;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import com.dru.nm.NannyMeterActivity;
 
@@ -13,11 +16,12 @@ ActivityInstrumentationTestCase2<NannyMeterActivity> {
 
 	private NannyMeterActivity mActivity;
 	private TextView mStartTimeView;
-	private String resourceString;
+	//	private String resourceString;
 	private EditText mRateView;
 	private EditText mTipView;
-	private TextView mRateLabel;
-	private TextView mTipLabel;
+	//	private TextView mRateLabel;
+	//	private TextView mTipLabel;
+	private ToggleButton mStartStopView;
 
 	public NannyMeterTest() {
 		super("com.dru.nm", NannyMeterActivity.class);
@@ -26,17 +30,19 @@ ActivityInstrumentationTestCase2<NannyMeterActivity> {
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		mActivity = this.getActivity();
-		mActivity.runOnUiThread( new Runnable() {
+		setActivityInitialTouchMode(false);
+		mActivity = getActivity();
+		myRunOnUiThread( new Runnable() {
 			public void run() {
 				mActivity.resetSavedPreferences();
 			} } ); 
 		mStartTimeView = (TextView) mActivity.findViewById(com.dru.nm.R.id.starttime);
 		mRateView = (EditText) mActivity.findViewById(com.dru.nm.R.id.rate);
-		mRateLabel = (TextView) mActivity.findViewById(com.dru.nm.R.id.rateLabel);
+		//	mRateLabel = (TextView) mActivity.findViewById(com.dru.nm.R.id.rateLabel);
 		mTipView   = (EditText) mActivity.findViewById(com.dru.nm.R.id.tip);
-		mTipLabel   = (TextView) mActivity.findViewById(com.dru.nm.R.id.tipLabel);
-		resourceString = mActivity.getString(com.dru.nm.R.string.nannymeter);
+		//	mTipLabel   = (TextView) mActivity.findViewById(com.dru.nm.R.id.tipLabel);
+		//	resourceString = mActivity.getString(com.dru.nm.R.string.nannymeter);
+		mStartStopView = (ToggleButton) mActivity.findViewById(com.dru.nm.R.id.startstop); 
 	}
 
 	public void testPreconditions() {
@@ -47,52 +53,89 @@ ActivityInstrumentationTestCase2<NannyMeterActivity> {
 		assertEquals("",(String)mStartTimeView.getText());
 	}
 
-	public void testFieldUI() {
+	private void myRunOnUiThread( final Runnable r) {
+		final CyclicBarrier barrier = new CyclicBarrier(2);
 		mActivity.runOnUiThread(
 				new Runnable() {
 					public void run() {
+						r.run();
+						try { barrier.await(); } catch (Exception e) { e.printStackTrace(); }
+					} 
+				});
+		try { barrier.await(); } catch (Exception e) { e.printStackTrace(); }
+	}
+
+	public void testFieldUI() {
+		myRunOnUiThread(
+				new Runnable() {
+					public void run() {
 						mRateView.requestFocus();
-						mRateView.setSelection(0,10);
-						sendKeys(KeyEvent.KEYCODE_DEL); // delete everything
-						sendKeys(KeyEvent.KEYCODE_ENTER);
+						mRateView.selectAll();
 					} 
 				} 
-				); 
-		assertTrue("a rate error was detected", mActivity.mRateValid == false );
-		assertTrue("startStop is disabled", mStartTimeView.isEnabled() == false);
+		); 
+		sendKeys(KeyEvent.KEYCODE_DEL); // delete everything
+		assertTrue("rate is deleted", mRateView.getText().length() == 0);
+		sendKeys(KeyEvent.KEYCODE_ENTER);
 
-		mActivity.runOnUiThread(
+		assertTrue("a rate error was detected", mActivity.mRateValid == false );
+    assertNotNull("Error Text is set on Rate", mRateView.getError());
+		assertTrue("startStop is disabled", mStartStopView.isEnabled() == false);
+
+		myRunOnUiThread(
 				new Runnable() {
 					public void run() {
 						mRateView.requestFocus();
 						mRateView.setSelection(0);
-						sendKeys("12.00"); // delete everything
-
-						mTipView.requestFocus();
-						mTipView.setSelection(0,10);
-						sendKeys(KeyEvent.KEYCODE_DEL);
-						mRateView.requestFocus();
 					} 
 				} 
-				); 
-		assertTrue("rate is okay", mActivity.mRateValid == true );
-		assertTrue("tip is invalid", mActivity.mTipValid == false );
-		assertTrue("startStop is disabled", mStartTimeView.isEnabled() == false);
+		); 
+		sendKeys("1 2 PERIOD 0 0"); // delete everything and set it to 12.00
+		assertTrue("rate is set to 12.00", mRateView.getText().toString().equals("12.00"));
 
-		mActivity.runOnUiThread(
+		myRunOnUiThread( new Runnable() {
+			public void run() {
+				mTipView.requestFocus();
+				mTipView.selectAll();
+			}
+		} );
+		// tip getting focus should trigger the validation logic
+		assertTrue("rate is okay", mActivity.mRateValid == true );
+    assertNull("Error text is unset on Rate", mRateView.getError());
+		sendKeys(KeyEvent.KEYCODE_DEL);
+		assertTrue("Tip is deleted", mTipView.getText().length() == 0);
+		myRunOnUiThread( new Runnable() {
+			public void run() {
+				mRateView.requestFocus();
+			}
+		} );
+
+		assertTrue("tip is invalid", mActivity.mTipValid == false );
+    assertNotNull("Error Text is set on Tip", mTipView.getError());
+		assertTrue("startStop is disabled", mStartStopView.isEnabled() == false);
+
+		myRunOnUiThread(
 				new Runnable() {
 					public void run() {
 						mTipView.requestFocus();
 						mTipView.setSelection(0);
-						sendKeys("8");
-						sendKeys(KeyEvent.KEYCODE_BACK);
 					} 
 				} 
-				); 
+		); 
+
+		sendKeys("8");
+		sendKeys(KeyEvent.KEYCODE_ENTER);
 
 		assertTrue("tip is okay", mActivity.mTipValid == true );
-		assertTrue("startStop is enabled", mStartTimeView.isEnabled() == true);
+    assertNull("Error text is unset on Tip", mTipView.getError());
+		assertTrue("startStop is enabled", mStartStopView.isEnabled() == true);
 	}
-
-
+    
+	public void testCalc() {
+	 
+	 long start = System.currentTimeMillis();
+   long end = start + 1*60*60*1000 + 1*60*1000 + 1*1000;  
+	 String elapsed = mActivity.getElapsedTimeString(start, end);
+	 assertEquals("elapsed is 1:01:01", elapsed, "1:01:01");
+	}
 }
